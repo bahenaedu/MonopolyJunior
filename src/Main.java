a6e0423a3ef8c50bff6a1ab5000a2c974e20533f
bahenaedu <ebahena@coe.edu>
2019-04-30 11:33:54 -0500

    Making progress

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.awt.desktop.SystemEventListener;
-import java.util.ArrayList;
-
 public class Main {
     //The Main just creates the object of MonopolyGame and call the play method nothing more.
     public static void main(String[] args) {
